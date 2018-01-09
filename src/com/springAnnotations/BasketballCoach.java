77ffbc03e88713084ff465df7f8b7fc38f368e3b
Vishesh <visheshsingh16@gmail.com>
2018-01-09 15:52:04 -0600

    Qualifiers for dependency injection

--- src/com/springAnnotations/BasketballCoach.java
+++ src/com/springAnnotations/BasketballCoach.java
@@ -1,18 +1,26 @@
 package com.springAnnotations;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component("mrJordanTheGreat")
 public class BasketballCoach implements Coach {
 
+	@Autowired
+	@Qualifier("happyFortuneService")
 	public FortuneService fortuneService;
 
-	// Define a constructor
-	@Autowired
-	public BasketballCoach(FortuneService theFortuneService) {
-		fortuneService = theFortuneService;
+	// default constructor
+	public BasketballCoach() {
+		System.out.println(">>BasketballCoach: inside the constructor");
 	}
+
+	// Define a constructor
+	// @Autowired
+	// public BasketballCoach(FortuneService theFortuneService) {
+	// fortuneService = theFortuneService;
+	// }
 
 	@Override
 	public String getDailyWorkout() {
--- src/com/springAnnotations/TennisCoach.java
+++ src/com/springAnnotations/TennisCoach.java
@@ -1,6 +1,7 @@
 package com.springAnnotations;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 //@Component("thatSillyCoach") - Explicit Bean id
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Component;
 public class TennisCoach implements Coach {
 
 	@Autowired // Field injection
+	@Qualifier("happyFortuneService")
 	public FortuneService fortuneService;
 
 	// Define a constructor for constructor injection
