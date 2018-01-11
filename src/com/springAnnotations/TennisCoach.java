206cfc70c15b1aefc8d997343736769199eb1926
Vishesh <visheshsingh16@gmail.com>
2018-01-10 20:43:31 -0600

    Testing prototype scope with annotations

--- src/com/springAnnotations/TennisCoach.java
+++ src/com/springAnnotations/TennisCoach.java
@@ -2,10 +2,12 @@ package com.springAnnotations;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 //@Component("thatSillyCoach") - Explicit Bean id
 @Component // Default Bean id
+@Scope("prototype")
 public class TennisCoach implements Coach {
 
 	@Autowired // Field injection
