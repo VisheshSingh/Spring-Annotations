84a8bd83628b9c0ae32e2a7091dea5523c8cf5ae
Vishesh <visheshsingh16@gmail.com>
2018-01-12 09:33:54 -0600

    Spring Configuration with Java Code

---
+++ src/com/springAnnotations/SportConfig.java
@@ -0,0 +1,10 @@
+package com.springAnnotations;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.springAnnotations")
+public class SportConfig {
+
+}
